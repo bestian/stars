30faf464ba6087b8ed91d951c5e48194cb5a8e3d
Bestian Tang 小巴 <bestian@gmail.com>
2022-08-06 12:44:08 +0800

    Update Ad.vue

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -2,7 +2,7 @@
   <div class="ad">
     <div class="ui divider"></div>
     <h3>喜歡這篇小說嗎?</h3>
-    <h4>掃此打賞作者$NT100元</h4>
+    <h4>用<a href="https://www.google.com/search?q=%E6%82%A0%E9%81%8A%E4%BB%98" target="_blank">悠遊付</a>掃此打賞作者$NT100元</h4>
     <img src="../assets/100.png" />
   </div>
 
